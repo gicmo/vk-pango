896b00c5d1e302785b139b0e8e509c16c90569c6
Christian Kellner <christian@kellner.me>
2017-07-15 11:41:25 +0200

    Refactor tests for skyline & guillotine packers

--- testbinpacker.c
+++ testbinpacker.c
@@ -8,23 +8,9 @@
 #include <cairo.h>
 #include <pango/pangocairo.h>
 
-typedef struct {
+typedef struct Fixture {
   int dummy;
 } Fixture;
-
-static void
-fixture_set_up (Fixture       *fixture,
-		gconstpointer  user_data)
-{
-
-}
-
-static void
-fixture_tear_down (Fixture       *fixture,
-		   gconstpointer  user_data)
-{
-
-}
 
 static void
 test_rect_basic (Fixture       *fixture,
@@ -77,10 +63,10 @@ test_rect_merge (Fixture       *fixture,
 
   a.x = a.y = 0;
 
+  a.height = a.width = b.height = b.width = 10;
+
   b.x = a.width + 10;
   b.y = a.height + 10;
-
-  a.height = a.width = b.height = b.width = 10;
 
   /* same size, but not overlapping or sharing a border */
   g_assert_false(g_rect_merge(&a, &b, &u));
@@ -192,41 +178,60 @@ test_rect_merge (Fixture       *fixture,
 
 }
 
+
+static const char sample_text[] =
+  "AbCdEfGhIjKlMnOpQrStUvWxYz"
+  "aBcDeFgHiJkLmNoPqRsTuVwXyZ"
+  "ÄäÖöÜüß"
+  "θαζχσωεδξωφρτγβνηψυςμκιολπάέήίϊΐόύϋΰώ"
+  "ŁĄŻĘĆŃŚŹ"
+  "ЯБГДЖЙ"
+  "ろぬふあうええおやゆよわほへたていすかんなにらぜ゜むちとしはきくまのりれけむつさそひこみもねるめ"
+  "غ ظ ض ذ خ ث ت ش ر ق ص ف ع س ن م ل ك ي ط ح ز و ه د ج ب ا شين"
+  "¥£€$¢₡₢₣₤₥₦₧₨₩₪₫₭₮₯₹"
+  "0123456789+*[]{}!\"#$%&'()~=";
+
 typedef struct GlyphInfo {
   PangoGlyph      glyph;
   PangoFont      *font;
   PangoRectangle  ink;
 } GlyphInfo;
 
+typedef struct {
+  cairo_t *cr;
+  cairo_surface_t *surface;
+  PangoLayout *layout;
+  PangoContext *context;
+  PangoFontMap *fontmap;
+  GArray *bins;
+
+  guint width;
+  guint height;
+} PackerFixture;
+
 static void
-test_font_basic (Fixture       *fixture,
-		 gconstpointer  user_data)
+fixture_set_up (PackerFixture *fixture,
+		gconstpointer  user_data)
 {
   cairo_surface_t *surface;
   cairo_t *cr;
-  cairo_scaled_font_t *scaled_font;
   cairo_format_t format_cairo;
   PangoLayout *layout;
   PangoFontDescription *desc;
-  cairo_status_t status;
   PangoRectangle pr = {0, };
   PangoContext *context;
   PangoFontMap *fontmap;
-  GGuillotinePacker *packer;
-  GArray *bins, *packed, *rfree;
+  GArray *bins;
   gint i;
   int scale = 2;
 
+  fixture->height = 256;
+  fixture->width = 256;
+
   format_cairo = CAIRO_FORMAT_ARGB32;
   surface = cairo_image_surface_create(format_cairo,
-                                       256*scale,
-                                       256*scale);
-
-  packer = g_object_new(G_TYPE_GUILLOTINE_PACKER,
-                        "width", 256,
-                        "height", 256,
-                        "merge-free", TRUE,
-                        NULL);
+                                       fixture->width * scale,
+                                       fixture->height * scale);
 
   cr = cairo_create(surface);
 
@@ -244,24 +249,13 @@ test_font_basic (Fixture       *fixture,
   layout = pango_layout_new(context);
 
   pango_layout_set_font_description(layout, desc);
+  pango_font_description_free(desc);
+
   pango_layout_set_text(layout,
-                        "AbCdEfGhIjKlMnOpQrStUvWxYz"
-                        "aBcDeFgHiJkLmNoPqRsTuVwXyZ"
-                        "ÄäÖöÜüß"
-                        "θαζχσωεδξωφρτγβνηψυςμκιολπάέήίϊΐόύϋΰώ"
-                        "ŁĄŻĘĆŃŚŹ"
-                        "ЯБГДЖЙ"
-                        "ろぬふあうええおやゆよわほへたていすかんなにらぜ゜むちとしはきくまのりれけむつさそひこみもねるめ"
-                        "غ ظ ض ذ خ ث ت ش ر ق ص ف ع س ن م ل ك ي ط ح ز و ه د ج ب ا شين"
-                        "¥£€$¢₡₢₣₤₥₦₧₨₩₪₫₭₮₯₹"
-                        "0123456789+*[]{}!\"#$%&'()~="
-                        ,
+                        sample_text,
                         -1);
 
   pango_layout_get_pixel_extents(layout, &pr, NULL);
-
-  g_print("layout\n x: %d, y: %d, w: %d, h: %d\n",
-          pr.x, pr.y, pr.width, pr.height);
 
   //pango_cairo_show_layout (cr, layout);
 
@@ -296,50 +290,45 @@ test_font_basic (Fixture       *fixture,
         gr.width = info->ink.width + 1;
         gr.id = info;
 
-        g_print("  %u: %d, %d | %d, %d, %d, %d\n",
-                glyph,
-                gr.height, gr.width,
-                info->ink.x, info->ink.y,
-                info->ink.height, info->ink.width);
-
         g_array_append_val(bins, gr);
       }
+
   } while (pango_layout_iter_next_run(li));
 
-  packed = g_guillotine_packer_insert(packer, bins);
-  g_object_get(packer,
-               "free-rects", &rfree,
-               NULL);
+  fixture->cr = cr;
+  fixture->surface = surface;
+  fixture->layout = layout;
+  fixture->context = context;
+  fixture->fontmap = fontmap;
+  fixture->bins = bins;
+}
 
-  g_print("Packing done [%u bins packed, %u free rects]\n",
-          packed->len, rfree->len);
+  static void
+fixture_tear_down (PackerFixture *fixture,
+		   gconstpointer  user_data)
+{
+  cairo_destroy(fixture->cr);
+  cairo_surface_destroy(fixture->surface);
+  g_object_unref(fixture->layout);
+  g_object_unref(fixture->context);
+  g_object_unref(fixture->fontmap);
+}
 
+static void
+draw_packed_bins(cairo_t *cr,
+                 GArray *packed)
+{
+  guint i;
 
-  for (i = 0; i < rfree->len; i++)
-    {
-      GRect *gr = &g_array_index(rfree, GRect, i);
-
-      g_print("  %u: %u, %u, %u, %d\n",
-              i,
-              gr->x, gr->y,
-              gr->width, gr->height);
-
-      cairo_set_line_width(cr, .5);
-      cairo_set_source_rgba(cr, 0.0, 0.0, 1.0, 0.1);
-      cairo_rectangle(cr, gr->x, gr->y, gr->width, gr->height);
-      cairo_fill_preserve(cr);
-      cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 0.1);
-      cairo_stroke(cr);
-    }
-
-
-  g_print("glyphs (packed) [%u]: \n", packed->len);
+  if (packed == NULL || packed->len < 1)
+    return;
 
   for (i = 0; i < packed->len; i++)
     {
       GRect *gr = &g_array_index(packed, GRect, i);
       GlyphInfo *info = gr->id;
       PangoGlyph glyph = info->glyph;
+      cairo_scaled_font_t *scaled_font;
       cairo_glyph_t cairo_glyph;
 
       cairo_glyph.index = glyph;
@@ -353,51 +342,147 @@ test_font_basic (Fixture       *fixture,
       cairo_show_glyphs(cr, &cairo_glyph, 1);
       cairo_restore (cr);
 
-      g_print("  %u: %d, %d, %d, %d @ %f, %f [%d, %d]\n",
-              glyph,
-              gr->x,
-              gr->y,
-              gr->width,
-              gr->height,
-              cairo_glyph.x,
-              cairo_glyph.y,
-              info->ink.x,
-              info->ink.y);
-
       cairo_set_line_width(cr, 1);
       cairo_set_source_rgba(cr, 0.0, 1.0, 0.0, 0.1);
       cairo_rectangle(cr, gr->x, gr->y, gr->width, gr->height);
       cairo_stroke_preserve(cr);
       cairo_fill(cr);
     }
+}
 
-  GArray *bad = g_guillotine_packer_check(packer);
+static void
+draw_rects(cairo_t *cr,
+           GArray  *rects,
+           double   r,
+           double   g,
+           double   b)
+{
+  guint i;
 
-  if (bad)
+  if (rects == NULL || rects->len < 1)
+    return;
+
+
+  for (i = 0; i < rects->len; i++)
     {
-      g_print("consistency error: %d intersections\n", bad->len);
-    }
-
-  for (i = 0; bad && i < bad->len; i++)
-    {
-      GRect *r = &g_array_index(bad, GRect, i);
+      GRect *gr = &g_array_index(rects, GRect, i);
 
       cairo_set_line_width(cr, .5);
-      cairo_set_source_rgba(cr, 1.0, 0.0, 0.0, 0.5);
-      cairo_rectangle(cr, r->x, r->y, r->width, r->height);
+      cairo_set_source_rgba(cr, r, g, b, 0.1);
+      cairo_rectangle(cr, gr->x, gr->y, gr->width, gr->height);
       cairo_fill_preserve(cr);
-      cairo_set_source_rgba(cr, 1.0, 0.0, 0.0, 0.1);
+      cairo_set_source_rgba(cr, r, g, b, 0.1);
       cairo_stroke(cr);
+    }
+}
 
+static void
+test_guillotine_packer (PackerFixture *fixture,
+                        gconstpointer  user_data)
+{
+  cairo_surface_t *surface;
+  cairo_t *cr;
+  GGuillotinePacker *packer;
+  cairo_status_t status;
+  GArray *packed, *rfree, *bad;
+
+  surface = fixture->surface;
+  cr = fixture->cr;
+
+  packer = g_object_new(G_TYPE_GUILLOTINE_PACKER,
+                        "width", fixture->width,
+                        "height", fixture->height,
+                        "merge-free", TRUE,
+                        NULL);
+
+  packed = g_guillotine_packer_insert(packer, fixture->bins);
+
+  g_object_get(packer,
+               "free-rects", &rfree,
+               NULL);
+
+  g_debug("Packing done [%u bins packed, %u free]\n",
+          packed->len, rfree->len);
+
+  draw_rects(cr, rfree, 0.0, 0.0, 1.0);
+  draw_packed_bins(cr, packed);
+
+  /* consistency check */
+  bad = g_guillotine_packer_check(packer);
+  draw_rects(cr, bad, 1.0, 0.0, 0.0);
+
+  cairo_surface_flush(surface);
+  status = cairo_surface_write_to_png(surface, "guillotine.png");
+
+  if (bad)
+    g_warning("consistency error: %d intersections\n", bad->len);
+
+  g_object_unref(packer);
+  g_assert_true(status == CAIRO_STATUS_SUCCESS);
+}
+
+static void
+draw_skyline(cairo_t *cr,
+             GArray  *skyline,
+             double   r,
+             double   g,
+             double   b)
+{
+
+  guint i;
+  GRect *n;
+
+  if (skyline == NULL || skyline->len < 1)
+    return;
+
+  n = &g_array_index(skyline, GRect, 0);
+
+  cairo_move_to(cr, 0, n->y);
+  cairo_set_line_width(cr, 1.0);
+  cairo_set_source_rgba(cr, r, g, b, 1.);
+
+  for (i = 0; i < skyline->len; i++)
+    {
+      GRect *n = &g_array_index(skyline, GRect, i);
+
+      cairo_line_to(cr, n->x,            n->y);
+      cairo_line_to(cr, n->x + n->width, n->y);
     }
 
-  cairo_destroy(cr);
+  cairo_stroke(cr);
+}
+
+static void
+test_skyline_packer (PackerFixture *fixture,
+                     gconstpointer  user_data)
+{
+  cairo_surface_t *surface;
+  cairo_t *cr;
+  GSkylinePacker *packer;
+  cairo_status_t status;
+  GArray *packed, *skyline;
+
+  surface = fixture->surface;
+  cr = fixture->cr;
+
+  packer = g_object_new(G_TYPE_SKYLINE_PACKER,
+                        "width", fixture->width,
+                        "height", fixture->height,
+                        NULL);
+
+  packed = g_skyline_packer_insert(packer, fixture->bins);
+  g_debug("Packing done [%u bins packed]\n",
+          packed->len);
+
+  draw_packed_bins(cr, packed);
+
+  g_object_get(packer, "skyline", &skyline, NULL);
+  draw_skyline(cr, skyline, 0.0, 0.0, 1.0);
+
   cairo_surface_flush(surface);
-  status = cairo_surface_write_to_png(surface, "fb-1.png");
+  status = cairo_surface_write_to_png(surface, "skyline.png");
 
-  cairo_surface_destroy(surface);
-  pango_font_description_free(desc);
-
+  g_object_unref(packer);
   g_assert_true(status == CAIRO_STATUS_SUCCESS);
 }
 
@@ -411,21 +496,27 @@ main (int argc, char **argv)
 
   g_test_add("/bin-packer/rect",
 	     Fixture, NULL,
-	     fixture_set_up,
+	     NULL,
 	     test_rect_basic,
-	     fixture_tear_down);
+	     NULL);
 
   g_test_add("/bin-packer/rect/merge",
 	     Fixture, NULL,
-	     fixture_set_up,
+	     NULL,
 	     test_rect_merge,
-	     fixture_tear_down);
+	     NULL);
 
-
-  g_test_add("/bin-packer/font-basic",
-             Fixture, NULL,
+  /* the packers */
+  g_test_add("/bin-packer/packer/guillotine",
+             PackerFixture, NULL,
              fixture_set_up,
-             test_font_basic,
+             test_guillotine_packer,
+             fixture_tear_down);
+
+  g_test_add("/bin-packer/packer/skyline",
+             PackerFixture, NULL,
+             fixture_set_up,
+             test_skyline_packer,
              fixture_tear_down);
 
   return g_test_run();
